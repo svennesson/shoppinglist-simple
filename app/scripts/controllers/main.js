3dca043885810b9a0b935dfa3bbc975a9fdae3a2
Marcus Svensson <svensson.marcus.9@gmail.com>
2015-10-04 11:41:28 +0200

    Initialized project with yeoman

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name shoppinglistSimpleApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the shoppinglistSimpleApp
+ */
+angular.module('shoppinglistSimpleApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
