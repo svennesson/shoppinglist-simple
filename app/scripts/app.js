208fdaf5dce4192a5f9af8fd0d08ebbbcf24190e
Marcus Svensson <svensson.marcus.9@gmail.com>
2015-10-04 17:23:45 +0200

    Removed unneccesary autogenerated code

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -17,16 +17,6 @@ angular
   ])
   .config(function ($routeProvider) {
     $routeProvider
-      .when('/', {
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl',
-        controllerAs: 'main'
-      })
-      .when('/about', {
-        templateUrl: 'views/about.html',
-        controller: 'AboutCtrl',
-        controllerAs: 'about'
-      })
       .otherwise({
         redirectTo: '/'
       });
